d30af53e1020c618414354a2800679639e38f209
Dongsu Jang <77693+iolo@users.noreply.github.com>
2026-01-01 15:55:12 +0900

    update demo images and applesoft basic code

--- demo/build-demo.sh
+++ demo/build-demo.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 
+# prerequisites:
 # cargo install a2kit
 # https://github.com/dfgordon/a2kit
-
-a2kit delete -d demo.dsk -f image.gr
-a2kit delete -d demo.dsk -f gr
-a2kit put -d demo.dsk -f image.gr -t bin -a 400 < image.gr
-a2kit tokenize -t atxt -a 2049 < gr.abas | a2kit put -d demo.dsk -t atok -f gr
-
-a2kit delete -d demo.dsk -f image.dgr
-a2kit delete -d demo.dsk -f dgr
-a2kit tokenize -t atxt -a 2049 < dgr.abas | a2kit put -d demo.dsk -t atok -f dgr
-a2kit put -d demo.dsk -f image.dgr -t bin -a 400 < image.dgr
-
-a2kit delete -d demo.dsk -f image.hgr
-a2kit delete -d demo.dsk -f hgr
-a2kit tokenize -t atxt -a 2049 < hgr.abas | a2kit put -d demo.dsk -t atok -f hgr
-a2kit put -d demo.dsk -f image.hgr -t bin -a 2000 < image.hgr
-
-a2kit delete -d demo.dsk -f image.dhgr
-a2kit delete -d demo.dsk -f dhgr
-a2kit tokenize -t atxt -a 2049 < dhgr.abas | a2kit put -d demo.dsk -t atok -f dhgr
-a2kit put -d demo.dsk -f image.dhgr -t bin -a 2000 < image.dhgr
-
-a2kit delete -d demo.dsk -f image.pixmap
-a2kit delete -d demo.dsk -f pixmap
-a2kit tokenize -t atxt -a 2049 < pixmap.abas | a2kit put -d demo.dsk -t atok -f pixmap
-a2kit put -d demo.dsk -f image.pixmap -t bin -a 2000 < image.pixmap
-
-a2kit delete -d demo.dsk -f image.bitmap
-a2kit delete -d demo.dsk -f bitmap
-a2kit tokenize -t atxt -a 2049 < bitmap.abas | a2kit put -d demo.dsk -t atok -f bitmap
-a2kit put -d demo.dsk -f image.bitmap -t bin -a 2000 < image.bitmap
-
-# on windows: https://github.com/AppleWin/AppleWin
+#
+# run on windows: https://github.com/AppleWin/AppleWin
 # applewin -d1 demo.dsk
-# on linux: https://github.com/audetto/AppleWin
+#
+# run on linux: https://github.com/audetto/AppleWin
 # sa2 --d1 demo.dsk
+#
 # ... or else
+
+OUTPUT=demo.dsk
+
+BIN_FILES=(
+  "image.gr"
+  "image.dgr"
+  "image.hgr"
+  "image.dhgr"
+  "i16x16.pixmap"
+  "i9x9.pixmap"
+  "i16x16.bitmap"
+  "i9x9.bitmap"
+)
+ABAS_FILES=(
+  "gr.abas"
+  "dgr.abas"
+  "hgr.abas"
+  "dhgr.abas"
+  "pixmap.abas"
+  "bitmap.abas"
+)
+
+add_bin() {
+  a2kit delete -d $OUTPUT -f $1
+  a2kit put -d $OUTPUT -f $1 -t bin -a 8192 < $1
+}
+
+add_abas() {
+  a2kit delete -d $OUTPUT -f $1
+  a2kit tokenize -t atxt -a 2049 < $1 | a2kit put -d $OUTPUT -t atok -f $1
+}
+
+for file in "${BIN_FILES[@]}"; do
+  add_bin "$file"
+done
+for file in "${ABAS_FILES[@]}"; do
+  add_abas "$file"
+done
+
+a2kit catalog -d $OUTPUT
+
