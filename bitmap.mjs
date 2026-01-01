b9b70e01e0eca43db17c70bfe58e9adc4d698484
Dongsu Jang <77693+iolo@users.noreply.github.com>
2026-01-01 15:54:56 +0900

    fix bitmap bit order bug

--- bitmap.mjs
+++ bitmap.mjs
@@ -20,10 +20,26 @@ export function init(width, height) {
   return new Uint8Array(Math.ceil((width * height) / 8));
 }
 
+// ex.
+// 000111000
+// 001000100
+// 010000010
+// 100000001
+// 100000001
+// 100000001
+// 010000010
+// 001000100
+// 000111000
+// -> 000111000 001000100 010000010 100000001 100000001 100000001 010000010 001000100 000111000
+// -> 00011100 00010001 00010000 01010000 00011000 00001100 00000101 00000100 01000100 00011100 0
+// -> 0x1C, 0x11, 0x10, 0x50, 0x18, 0x0C, 0x05, 0x04, 0x44, 0x1C, 0x00
+// w=9,h=9
+// x=3,y=0 -> index=3 -> byte 0, bit 4 -> bitmask 0x10
 export function setPixel(fb, x, y, color) {
   const index = y * fbWidth + x;
   const offset = index >> 3;
-  const mask = 1 << (index & 7);
+  const mask = 1 << (7 - (index % 8));
+  //const mask = 1 << (index & 7);
   if (color) {
     fb[offset] |= mask;
   } else {
@@ -34,6 +50,7 @@ export function setPixel(fb, x, y, color) {
 export function getPixel(fb, x, y) {
   const index = y * fbWidth + x;
   const offset = index >> 3;
-  const mask = 1 << (index & 7);
+  const mask = 1 << (7 - (index % 8));
+  //const mask = 1 << (index & 7);
   return fb[offset] & mask ? 1 : 0;
 }
